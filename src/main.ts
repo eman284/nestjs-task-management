5b2ae47c3d0dba10e9246c584d61ada691ac716f
Iman Hussien <a.ihussien@dev.thiqah.sa>
2021-12-15 09:35:20 +0200

    add dynamic port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
   await app.listen(3000);
 }
 
