3a1d0bbae6fa918d34d2272e50931332ff5b11e7
Iman Hussien <a.ihussien@dev.thiqah.sa>
2021-12-15 09:47:46 +0200

    modify package file

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
   const config = new DocumentBuilder()
     .setTitle("Task Management")
     .setDescription("The task API description")
@@ -14,9 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
-  await app.listen(3000);
 }
 
 bootstrap();
