f9b920d4877eddcc44beda364cf9b158a0898789
Iman Hussien <a.ihussien@dev.thiqah.sa>
2021-12-19 22:30:51 +0200

    modify swagger

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,40 @@ import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { TransformInterceptor } from "./transform.interceptor";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
-  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-  const config = new DocumentBuilder()
+  const logger = new Logger();
+  const configService = app.get(ConfigService);
+  const port = configService.get("PORT");
+  const isDevelopment = configService.get("NODE_ENV") === "development";
+
+  app.enableCors();
+
+  const documentBuilder = new DocumentBuilder()
     .setTitle("Task Management")
-    .setDescription("The task API description")
-    .setExternalDoc("/api-json", "/api-json")
-    .addBearerAuth()
-    .addServer(`http://localhost:${port}`)
+    .setDescription("The Task Management API")
     .setVersion("1.0")
-    .build();
+    .setExternalDoc("api-json", "api-json")
+    .addBasicAuth();
+
+  documentBuilder
+    .addServer("https://task-management-nestjsapi.herokuapp.com");
+  if (isDevelopment) {
+    documentBuilder
+      .addServer(`http://localhost:${port}`);
+  }
+
+  const config = documentBuilder.build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  app.enableCors();
+
   await app.listen(port);
+
   logger.log(`Application listen in port ${port}`);
 }
 
